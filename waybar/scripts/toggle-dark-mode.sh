764356e4c09c227a87324196a888239befb5cdfe
ChaosVariable <brudna.hara@gmail.com>
2026-04-16 19:02:11 +0200

    Delete waybar/scripts/toggle-dark-mode.sh

--- waybar/scripts/toggle-dark-mode.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-current_theme=$(gsettings get org.gnome.desktop.interface gtk-theme)
-if [[ "$current_theme" == *"Dark"* ]]; then
-    gsettings set org.gnome.desktop.interface gtk-theme "Adwaita"
-    echo "🌞"
-else
-    gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark"
-    echo "🌙"
-fi
